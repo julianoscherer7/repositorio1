bf99f918e4c9901bf30ad59028b1a247a9122255
julianoscherer7 <162385579+julianoscherer7@users.noreply.github.com>
2025-03-12 00:12:01 +0000

    a

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard inicial</title>
+    <style>
+        body{
+        font-family: Helvetica;
+        dipslay: flex;
+        margin: 0;
+    }
+
+</head>
+<body>
+    <form action="a.php" method="post">
+        <input type="text" name="none" placeholder="Digite seu nome" />
+        <input type="submit" value="Enviar">
+</body>
+</html>
